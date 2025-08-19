e9fb0497b3e2282f088d2ccae729170791681938
TheMasterGame0 <112582094+TheMasterGame0@users.noreply.github.com>
2025-08-19 12:12:45 -0300

    :sparkles: Incremento na especificação

--- src/main/java/org/caixa/Rest/SimulacaoRest.java
+++ src/main/java/org/caixa/Rest/SimulacaoRest.java
@@ -28,7 +28,10 @@ public class SimulacaoRest {
   @GET
   @Path("/simulacoes")
   public Response simulacoesAnteriores(FiltroDTO dados) {
-    // Logica para calculo do SAC e do PRICE
+    // Acessar o Banco local para consultar todas as simulações feitas
+
+    // Regra de negócio -> salvar o valor Total como sendo o menor valor calculado
+    // Possibilidade, incluir no processo de simulação opção de especificar a simulação e limitar o valor salvo 
 
     // Definir DTO pra saida
 
